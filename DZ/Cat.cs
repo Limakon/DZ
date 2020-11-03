03ac8121ccc33ce6132d8fcf6cadcddba58b4830
Дмитрий Булатников <bulatnikov_2002@mail.ru>
2020-11-03 21:50:16 +1000

    Добавьте файлы проекта.

---
+++ DZ/Cat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DZ
+{
+    class Cat
+    {
+        public Cat(string name, DateTime berthday)
+        {
+
+            Name = name;
+            Berthday = berthday;
+
+
+        }
+
+        public string Name
+        {
+            get;
+
+            set;
+        }
+        public void MakeNoise()
+        {
+
+            Console.WriteLine($"{Name} мяукает");
+        }
+        public DateTime Berthday
+        {
+            get;
+
+            set;
+
+        }
+        public int GetAge()
+        {
+            return (DateTime.Today - Berthday).Days / 365;
+
+        }
+    }
+}
---
+++ DZ/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DZ
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+
+            Cat c = new Cat("Kot", new DateTime(2001, 01, 01));
+            c.MakeNoise();
+            Console.WriteLine($"Коту {c.Name} {c.GetAge()} лет");
+
+        }
+    }   }
